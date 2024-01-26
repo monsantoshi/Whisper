bc068a4a8e4e948c7c4b6a4cea4c7d3e5ebe3886
monsantoshi <monsan@gmail.com>
2024-01-26 16:05:47 +0700

    cc

--- backup12/main.py
+++ backup12/main.py
@@ -496,6 +496,6 @@ def main():
 
 if __name__ == "__main__":
     load_dotenv()
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+    openai.api_key = st.secrets['OPENAI_API_KEY']
     main()
     
